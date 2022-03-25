3cb5d29dc89ed6464ca5a5ca10d53a1266957932
ErinWeisbart <eweisbar@broadinstitute.org>
2022-03-25 13:05:36 -0700

    more path fixing

--- worker/B2R-worker.py
+++ worker/B2R-worker.py
@@ -132,7 +132,7 @@ def runSomething(message):
     # Build and run your program's command
     # ie cmd = my-program --my-flag-1 True --my-flag-2 VARIABLE
     # you should assign the variable "localOut" to the output location where you expect your program to put files
-    cmd = f'bioformats2raw {message['input_location']}/{message['Plate']}/{message['path_to_metadata']} {message['input_location']}/images_zarr/{message['Plate']}.ome.zarr --resolutions {message['resolutions']}'
+    cmd = f'bioformats2raw {message['input_location']}/images/{message['Plate']}/{message['path_to_metadata']} {message['input_location']}/images_zarr/{message['Plate']}.ome.zarr --resolutions {message['resolutions']}'
 
     print('Running', cmd)
     logger.info(cmd)
@@ -150,7 +150,7 @@ def runSomething(message):
         while mvtries <3:
             try:
                     printandlog('Move attempt #'+str(mvtries+1),logger)
-                    cmd = 'aws s3 mv ' + localOut + ' s3://' + AWS_BUCKET + '/' + message['output'] + ' --recursive'
+                    cmd = f'aws s3 mv {localOut} s3://{AWS_BUCKET}/{message['input']}/images_zarr --recursive'
                     subp = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     out,err = subp.communicate()
                     out=out.decode()
